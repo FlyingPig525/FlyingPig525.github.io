3d5949698ea9a8fb1826780eaf8af65f79de2a6f
FlyingPig525 <66709612+FlyingPig525@users.noreply.github.com>
2023-08-01 19:52:05 -0400

    wgat

--- JS/clicker.js
+++ JS/clicker.js
@@ -19,12 +19,20 @@
 */
 const buttonGrey = 'rgb(126, 126, 126)';
 const bodyGrey = 'rgb(63, 63, 63)';
+const subGrey = 'rgb(202, 202, 202)';
 const d = document;
 const darkLight = d.getElementById('darkLight');
 const btn = d.querySelector('button');
 const moneyCountText = d.getElementById('moneyCount');
 const IncUpgrade = d.getElementById('incUpgrade');
 const autoClickUpgrade = d.getElementById('autoUpgrade');
+const moneyPerSecText = d.getElementById('moneyPerSec');
+
+const autoClickerAction = setInterval(function () {
+  money += autoClickers * moneyInc;
+  moneyCountText.innerHTML = 'Ω' + Math.round(money);
+  moneyPerSecText.innerHTML = `Ω${autoClickers * moneyInc * 5} per second`;
+}, 200);
 
 let currentMode = 'dark';
 let money = 0;
@@ -32,65 +40,76 @@ let moneyInc = 1;
 let incCost = 200;
 let autoClickCost = 3000;
 let autoClickers = 0;
-let autoClickerAction = setInterval(function() {
-    money += autoClickers * moneyInc;
-    console.log('+' + (moneyInc * autoClickers));
-    moneyCountText.innerHTML = 'Ω' + Math.round(money);
-}, 200);
-
 
 function loadPage() {
-    moneyCountText.innerHTML = 'Ω' + Math.round(money);
-    autoClickerAction;
+  moneyCountText.innerHTML = 'Ω' + Math.round(money);
+  autoClickerAction;
 }
 
 d.addEventListener('load', loadPage());
 
 function darkToLight() {
-    if (currentMode === 'dark') {
-        currentMode = 'light';
-        btn.style.backgroundColor = buttonGrey;
-        btn.style.color = 'white';
-        darkLight.innerHTML = 'Dark Mode'
-        d.querySelector('body').style.backgroundColor = 'white';
-        d.querySelector('h1').style.color = 'black';
-    } else {
-        currentMode = 'dark';
-        btn.style.backgroundColor = 'white';
-        btn.style.color = buttonGrey;
-        darkLight.innerHTML = 'Light Mode'
-        d.querySelector('body').style.backgroundColor = bodyGrey;
-        d.querySelector('h1').style.color = 'white';
-    }
+  if (currentMode === 'dark') {
+    currentMode = 'light';
+    btn.style.backgroundColor = buttonGrey;
+    btn.style.color = 'white';
+    darkLight.innerHTML = 'Dark Mode';
+    d.querySelector('body').style.backgroundColor = 'white';
+    d.querySelector('h1').style.color = 'black';
+  } else {
+    currentMode = 'dark';
+    btn.style.backgroundColor = 'white';
+    btn.style.color = buttonGrey;
+    darkLight.innerHTML = 'Light Mode';
+    d.querySelector('body').style.backgroundColor = bodyGrey;
+    d.querySelector('h1').style.color = 'white';
+  }
 }
 
 function moneyClick() {
-    money += moneyInc;
-    moneyCountText.innerHTML = 'Ω' + Math.round(money);
+  money += moneyInc;
+  moneyCountText.innerHTML = 'Ω' + Math.round(money);
 }
 
 function incUpgrade() {
-    if (money >= incCost) {
-        money -= incCost;
-        incCost *= 2;
-        moneyInc += Math.floor((moneyInc * 1.1));
-        IncUpgrade.innerHTML = 'Increment The Increment <br>Costs: Ω' + Math.round(incCost);
-        moneyCountText.innerHTML = 'Ω' + Math.round(money);
-        console.log('Inc Amount: ' + moneyInc);
-    } else {
-        alert('Not Enough Ωs!');
-    }
+  if (money >= incCost) {
+    money -= incCost;
+    incCost *= 2;
+    moneyInc += Math.floor(moneyInc * 1.1);
+    IncUpgrade.innerHTML =
+      'Increment The Increment <br>Costs: Ω' + Math.round(incCost);
+    moneyCountText.innerHTML = 'Ω' + Math.round(money);
+    console.log('Inc Amount: ' + moneyInc);
+  } else {
+    alert('Not Enough Ωs!');
+  }
 }
 
 function autoUpgrade() {
-    if (money >= autoClickCost) {
-        money -= autoClickCost;
-        autoClickCost += Math.ceil(autoClickCost * 1.6);
-        autoClickers++;
-        autoClickUpgrade.innerHTML = 'Purchase an Auto Clicker <br>Costs: Ω' + autoClickCost;
-        moneyCountText.innerHTML = 'Ω' + Math.round(money);
-        console.log('Auto Clickers: ' + autoClickers);
-    } else {
-        alert('Not Enough Ωs!');
-    }
+  if (money >= autoClickCost) {
+    money -= autoClickCost;
+    autoClickCost += Math.ceil(autoClickCost * 1.6);
+    autoClickers++;
+    autoClickUpgrade.innerHTML =
+      'Purchase an Auto Clicker <br>Costs: Ω' + autoClickCost;
+    moneyCountText.innerHTML = 'Ω' + Math.round(money);
+    console.log('Auto Clickers: ' + autoClickers);
+  } else {
+    alert('Not Enough Ωs!');
+  }
+}
+
+function save() {
+  let jsonData = {};
+  jsonData.money = money;
+  jsonData.autoClickers = autoClickers;
+  jsonData.moneyInc = moneyInc;
+  jsonData.autoClickCost = autoClickCost;
+  jsonData.incCost = incCost;
+  encodedJSON = btoa(JSON.stringify(jsonData));
+  console.log(encodedJSON, jsonData);
+  d.querySelector('body').innerHTML = encodedJSON;
+  d.querySelector('body').style.color = 'white';
+  d.querySelector('body').style.fontSize = '2rem';
+  d.querySelector('body').style.wordBreak = 'break-word';
 }
